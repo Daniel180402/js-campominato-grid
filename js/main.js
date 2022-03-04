9d2933c20770cc2bf8c466bbbdc88997fdf0b56f
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-03-04 15:44:55 +0100

    added the opportunity to select the difficulty

--- js/main.js
+++ js/main.js
@@ -1,24 +1,50 @@
 // creazione griglia
-
 const gridElement = document.getElementById("grid");
+let difficulty = document.getElementById("difficulty")
+let buttonplay = document.getElementById("btnplay")
 
-const createGridSquare = () => {
-    const currentElement = document.createElement("div");
-    currentElement.classList.add("square-element-big");
+buttonplay.addEventListener("click", function(){
 
-    return currentElement
-}
+    const createGridSquare = () => {
+        const currentElement = document.createElement("div");
+        
+        if(difficulty.value == "facile"){
+            currentElement.classList.add("square-element-big");
+        }
+        else if(difficulty.value == "intermedio"){
+            currentElement.classList.add("square-element-medium");
+        }
+        else{
+            currentElement.classList.add("square-element-small");
+        }
+        
+    
+        return currentElement
+    }
+    
+    if(difficulty.value == "facile"){
+        matchDifficulty = 100;
+    }
+    else if(difficulty.value == "intermedio"){
+        matchDifficulty = 81;
+    }
+    else{
+        matchDifficulty = 49;
+    }
 
-for (let i = 1; i <= 100 ; i++){
-    // creazione singolo quadrato
-    const currentSquare = createGridSquare();
-    currentSquare.innerHTML = [i];
-    currentSquare.addEventListener("click", function(){
-        console.log(this);
-        this.classList.toggle("active");
-    })
+    for (let i = 1; i <= matchDifficulty ; i++){
+        // creazione singolo quadrato
+        const currentSquare = createGridSquare();
+        currentSquare.innerHTML = [i];
+        currentSquare.addEventListener("click", function(){
+            console.log(this);
+            this.classList.toggle("active");
+        })
+    
+        //viene aggiunto al suo parent
+        gridElement.appendChild(currentSquare);
+    
+    }
 
-    //viene aggiunto al suo parent
-    gridElement.appendChild(currentSquare);
+})
 
-}
